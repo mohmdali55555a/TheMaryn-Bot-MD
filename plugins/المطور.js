3992654988b4656b195a38cbfb910fba6d14a011
mohmdali555555534 <lywnmalak@gmail.com>
2024-05-22 12:25:35 +0300

    Update المطور.js

--- plugins/المطور.js
+++ plugins/المطور.js
@@ -2,7 +2,7 @@ function handler(m) {
   let data = global.owner.filter(([id, isCreator]) => id && isCreator)
   this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 }
-handler.alias = ['المطور']
+handler.alias = ['فحلك']
 handler.command = /^(المالك|المطور)$/i
 
 export default handler
