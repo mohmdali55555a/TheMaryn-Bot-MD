70c9fe65d8e8081d2b3441d5247c07166fae95cb
mohmdali555555534 <lywnmalak@gmail.com>
2024-05-22 07:43:41 +0300

    بوت.js

--- plugins/بوت.js
+++ plugins/بوت.js
@@ -1,1 +1,106 @@
-*مرحبا كيف يمكنني مساعدتك؟*
+جلب الاستيراد من "node-fetch"؛
+
+دع النقاط = 50؛
+السماح لـ maxPlayers؛
+
+دع المعالج = غير متزامن (م، {كون، الأمر، النص }) => {
+    اسمحوا معرف = m.chat;
+    conn.tekateki1 = conn.tekateki1 ? conn.tekateki1 : {};
+
+
+    إذا (command === "مسابقه") {
+        إذا (المعرف في conn.tekateki1) {
+            return conn.reply(m.chat, 'المسابقه شغاله ينجم', conn.tekateki1[id][0]);
+        } وإلا إذا (!نص) {
+            return conn.reply(m.chat, 'ادخل عدد اللاعبين', m);
+        } وإلا إذا (isNaN(نص)) {
+            return conn.reply(m.chat, 'يرجى إدخال رقم لعدد اللاعبين', m);
+        } وإلا إذا (نص > 8 || نص < 5) {
+            return conn.reply(m.chat, 'الحد الأقصى جزئين اثنين, جزئيا خمسه', m);
+        } آخر {
+            maxPlayers = text;
+        }
+        conn.tekateki1[المعرف] = [
+            انتظار conn.reply(m.chat, '1 - جاوب علي السوأل بسرعه\n2 - جمع 50 نقطه\n3 - اتبع التعليمات', m), [], [], 0
+        ];
+
+        conn.reply(m.chat, `🎡| بدأت أوت، يمكن لـ ${maxPlayers} الانضمام. اكتب " .انضمام" انضمام `, m);
+        رمي كاذبة.
+    } else if (command === "انضمام") {
+        إذا (!(المعرف في conn.tekateki1)) {
+            throw conn.reply(m.chat, 'لا يوجد مسابقة قائمة حاليا!', m);
+        }
+
+        إذا (conn.tekateki1[id][2].length >= maxPlayers) {
+            throw conn.reply(m.chat, 'اكتمل العدد', m);;
+        }
+
+        إذا (conn.tekateki1[id][2].findIndex(player => player.id === m.sender) !== -1) {
+            throw conn.reply(m.chat, 'أنت مسجل بالفعل', m);
+        }
+
+        conn.tekateki1[id][2].push({ id: m.sender, points: 0 });
+        conn.reply(m.chat, 'تم التسجيل الفعال!', m);
+
+        إذا (conn.tekateki1[id][2].length >= maxPlayers) {
+            دع tekateki1 = انتظر (انتظر الجلب(`https://raw.githubusercontent.com/miku-655/-/main/dd.json`)).json();
+            Let json = tekateki1[Math.floor(Math.random() * tekateki1.length)];
+            conn.tekateki1[id][1] = json;
+            دع اللاعبينList = conn.tekateki1[id][2].map((player, i) => `${i + 1} - @${player.id.split('@')[0]} [${ player.points} نقطة]`).join('\n');
+            Let question = `السؤال: ${json.question}`;//\n\n${playersList}
+            conn.reply(m.chat, question, m);
+        }
+    } else if (command === "حذف-مسابقه") {
+        إذا (!conn.tekateki1[id]) {
+            conn.reply(m.chat, 'لـم تـبـدأ الـمـبـاره بـعـد', m);
+        } آخر {
+            حذف conn.tekateki1[id];
+            conn.reply(m.chat, 'تـم حـذف الـلـعـبـه بـنـجـاح', m);
+        }
+    }
+};
+
+Handler.before = وظيفة غير متزامنة (m, { conn }) {
+  اسمحوا معرف = m.chat;
+  this.tekateki1 = this.tekateki1 ? this.tekateki1 : {};
+
+  if (!(id in this.tekateki1)) return;
+
+  Let json = this.tekateki1[id][1];
+  دع اللاعبين = this.tekateki1[id][2];
+  Let questionCount = this.tekateki1[id][3];
+
+  إذا (json && json.response && m.text.toLowerCase() === json.response.toLowerCase()) {
+      Let playerIndex = player.findIndex(player => player.id === m.sender);
+      اللاعبين[playerIndex].points += نقاط;
+      questionCount++;
+
+      إذا (اللاعبين. الطول === 2) {
+          دع الفائز = اللاعبين[playerIndex];
+          this.reply(m.chat, `المسابقة! الفائز هو @${winner.id.split('@')[0]} بـ ${winner.points} نقطة.`, m, { الإشارات: [winner. بطاقة تعريف] })؛
+          احذف this.tekateki1[id];
+      } آخر {
+          // إرسال قائمة باللاعبين أخيرًا
+          دعونا اللاعبينList = اللاعبين.خريطة((player, i) => `${i + 1} - @${player.id.split('@')[0]} [${player.points} نقطة]`) .join('\n');
+          this.reply(m.chat, `@${m.sender.split('@')[0]} أجاب بشكل صحيح! لا يمكنك الآن استبعاد لاعب عن طريق كتابة "#" متبوعًا برقم ترتيب اللاعب.\n\nاللاعبون المتبقون :\n\n${playersList}`, m, { الإشارات: conn.parseMention(playersList) });
+      }
+  } else if (m.text.startsWith("#") && player.length > 2) {
+      Let playerIndex = parseInt(m.text.replace("#", "")) - 1;
+      إذا (playerIndex < player.length && playerIndex !== player.findIndex(player => player.id === m.sender)) {
+          player.splice(playerIndex, 1);
+          دعونا اللاعبينList = اللاعبين.خريطة((player, i) => `${i + 1} - @${player.id.split('@')[0]} [${player.points} نقطة]`) .join('\n');
+          this.reply(m.chat, `تم استبعاد العضو. اللاعبون المتبقون:\n\n${playersList}`, m, {mns: conn.parseMention(playersList) });
+          دع tekateki1 = انتظر (انتظر الجلب(`https://raw.githubusercontent.com/miku-655/-/main/dd.json`)).json();
+          json = tekateki1[Math.floor(Math.random() * tekateki1.length)];
+          this.tekateki1[id][1] = json;
+          Let question = `السؤال: ${json.question}`;
+          conn.reply(m.chat, question, m);
+      } آخر {
+          conn.reply(m.chat, 'رقم تاريخ غير صحيح أو يمتنع عن نفسك', m);
+      }
+  }
+};
+
+Handler.command = /^(مسابقه|انضمام|حذف-مسابقه)$/i;
+
+معالج التصدير الافتراضي؛
