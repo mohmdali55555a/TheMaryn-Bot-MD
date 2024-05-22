1bb86f432f90656c6764e366ac01b1ff3857ed1e
mohmdali555555534 <lywnmalak@gmail.com>
2024-05-22 09:08:41 +0300

    Create bot.js

---
+++ plugins/bot.js
@@ -0,0 +1,24 @@
+```
+@bot
+[name]
+
+[description]
+
+**Commands:**
+
+* **[command name]**
+    * **Description:** [command description]
+    * **Usage:** [command usage instructions]
+    * **Example:** [command example]
+* ... (additional commands)
+
+**Usage:**
+
+* To start the bot, use the `/start` command.
+* To get help, use the `/help` command.
+
+**Additional Information:**
+
+* [Website](https://[website url])
+* [Documentation](https://[documentation url])
+* [Privacy Policy](https://[privacy policy url])
