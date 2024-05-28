6716516067bddee59c0e20a788c775f0c84a72d2
mohmdali555555534 <lywnmalak@gmail.com>
2024-05-28 10:15:38 +0300

    Update انطق.js

--- plugins/انطق.js
+++ plugins/انطق.js
@@ -1,44 +1,38 @@
-import gtts from 'node-gtts';
-import {readFileSync, unlinkSync} from 'fs';
-import {join} from 'path';
-const defaultLang = 'ar';
-const handler = async (m, {conn, args, usedPrefix, command}) => {
-  let lang = args[0];
-  let text = args.slice(1).join(' ');
-  if ((args[0] || '').length !== 2) {
-    lang = defaultLang;
-    text = args.join(' ');
-  }
-  if (!text && m.quoted?.text) text = m.quoted.text;
-  let res;
-  try {
-    res = await tts(text, lang);
-  } catch (e) {
-    m.reply(e + '');
-    text = args.join(' ');
-    if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] فين الكلام الي هنطقه \nمثال: ${usedPrefix + command} مرحبا*`;
-    res = await tts(text, defaultLang);
-  } finally {
-    if (res) conn.sendFile(m.chat, res, 'tts.opus', null, m, true);
-  }
-};
-handler.help = ['tts <lang> <teks>'];
-handler.tags = ['tools'];
-handler.command = /^انطق$/i;
-export default handler;
-
-function tts(text, lang = 'ar') {
-  console.log(lang, text);
-  return new Promise((resolve, reject) => {
-    try {
-      const tts = gtts(lang);
-      const filePath = join(global.__dirname(import.meta.url), '../tmp', (1 * new Date) + '.wav');
-      tts.save(filePath, text, () => {
-        resolve(readFileSync(filePath));
-        unlinkSync(filePath);
-      });
-    } catch (e) {
-      reject(e);
-    }
-  });
+import gtts from  'node-gtts'
+import { readFileSync, unlinkSync } from  'fs' 
+import { join } from  'path'
+const defaultLang =  'ar'
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+let lang = args[0]
+let text = args.slice(1).join(' ')
+if ((args[0] ||'').length !== 2) {
+lang = defaultLang
+text = args.join('')
 }
+if (!text && m.quoted?.text) text = m.quoted.text
+let res
+try { res = await tts(text, lang) }
+catch (e) {
+m.reply(e +'')
+text = args.join('')
+if (!text) throw `تحويل نص لاوديو مثال \n *.tts الملكي أحسن بوت في العالم*`
+res = await tts(text, defaultLang)
+} finally {
+if (res) conn.sendFile(m.chat, res,  'tts.opus', null, m, true)
+}}
+handler.help = ['tts <lang> <teks>']
+handler.tags = ['tools']
+handler.command = /^g?انطق$/i
+export default handler
+function tts(text, lang ='ar') {
+console.log(lang, text)
+return new Promise((resolve, reject) => {
+try {
+let tts = gtts(lang)
+let filePath = join(global.__dirname(import.meta.url),  '../tmp', (1 * new Date) +  '.wav')
+tts.save(filePath, text, () => {
+resolve(readFileSync(filePath))
+unlinkSync(filePath)
+})
+} catch (e) { reject(e) }
+})}
