ac8b06643893f5f22ba77ba2bbf60553c6bf722b
mohmdali555555534 <lywnmalak@gmail.com>
2024-05-30 03:42:54 +0300

    Create مخنوث.js

---
+++ plugins/مخنوث.js
@@ -0,0 +1,16 @@
+import util from 'util'
+import path from 'path'
+let user = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let k = Math.floor(Math.random() * 70);
+let top = `*${user(a)} انــت هــو الخنيث الكبير لهذي الشله ادعي من الله ان يهديك وتبطل مص😫*`.trim()
+conn.sendFile (m.reply (top, null, { mentions: [a]}))}
+handler.help = handler.command = ['مخنوث']
+handler.tags = ['fun']
+handler.group = true
+handler.limit = 0
+export default handler
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]}
