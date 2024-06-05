1af855683e304a1d34bb1d13c18a0c1485822775
mohmdali555555534 <lywnmalak@gmail.com>
2024-06-05 14:36:06 +0300

    Update زوجنيه.js

--- plugins/زوجنيه.js
+++ plugins/زوجنيه.js
@@ -6,14 +6,14 @@ let user = a =>  @  + a.split( @ )[0]
 let couples = {}
 
 function handler(m, { groupMetadata, command, conn, text, usedPrefix}) {
-  if (command ===  .زوجني ) {
+  if (command ===  ('زوجني") {
     let ps = groupMetadata.participants.map(v => v.id)
     let a = pickRandom(ps)
     let k = Math.floor(Math.random() * 70)
     let top = `*${user(a)} هذه هي زوجتك* 💍`.trim()
     couples[m.sender] = a
     conn.sendFile(m.reply(top, null, { mentions: [a]}))
-  } else if (command ===  .طلقني ) {
+  } else if (command ===  ( 'طلقني' ) {
     if (couples[m.sender]) {
       let ex = couples[m.sender]
       let top = `*${user(m.sender)} تم طلاقك من ${user(ex)}* 💔`.trim()
