84dbe94cae4eddb587e3a2cb29e492f26a8570f6
mohmdali555555534 <lywnmalak@gmail.com>
2024-06-08 11:13:13 +0300

    Create زخرفه.js

---
+++ plugins/زخرفه.js
@@ -0,0 +1,38 @@
+function handler(m, { text }) {
+  let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+  m.reply(teks.replace(/[a-z]/gi, v => {
+      return { 
+          'a': '𝐀',
+          'b': '𝐁',
+          'c': '𝐂',
+          'd': '𝐃',
+          'e': '𝐄',
+          'f': '𝐅',
+          'g': '𝐆',
+          'h': '𝐇',
+          'i': '𝐈',
+          'j': '𝐉',
+          'k': '𝐊',
+          'l': '𝐋',
+          'm': '𝐌',
+          'n': '𝐍',
+          'o': '𝐎',
+          'p': '𝐏',
+          'q': '𝐐',
+          'r': '𝐑',
+          's': '𝐒',
+          't': '𝐓',
+          'u': '𝐔',
+          'v': '𝐕',
+          'w': '𝐖',
+          'x': '𝐗',
+          'y': '𝐘',
+          'z': '𝐙', 
+      }[v.toLowerCase()] || v
+  }))
+}
+handler.help = ['H A R L E Y']
+handler.tags = ['H A R L E Y']
+handler.command =  /^(زخرفه|زخرفة)$/i
+
+export default handler
