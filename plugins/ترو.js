2bda8660aad644b64022a0b9a341f6b01ec2fc6d
mohmdali555555534 <lywnmalak@gmail.com>
2024-06-11 17:43:04 +0300

    Update ترو.js

--- plugins/ترو.js
+++ plugins/ترو.js
@@ -1,53 +1,33 @@
-const canal2 = 'https://telegra.ph/file/078014e17aedf4f7a6cd9.jpg'; 
-// COMBINACIÓN DE MENSAJES
-// Adaptar el simple.js
-let handler = async (m, { conn, usedPrefix, command, text }) => {
+import fetch from 'node-fetch'
+let handler = m => m
 
-// MENSAJE CARUSEL CON TODOS LOS BOTONES DISPONIBLES
-// Si las ids no te funciona con usedPrefix, tendrás que definirlas, ejemplo /menu
-const sections = [{
-title: `Título de la sección`,
-rows: [
-{ header: 'Encabezado1', title: "Título1", description: 'Descripción1', id: usedPrefix + "menu" }, 
-{ header: 'Encabezado2', title: "Título2", description: 'Descripción2', id: "Id2" }, 
-{ header: 'Encabezado3', title: "Título3", description: 'Descripción3', id: "Id3" }, 
-{ header: 'Encabezado4', title: "Título4", description: 'Descripción4', id: "Id4" }, 
-]},]  
-const messages = [[ // CARRUSEL 1
-'Descripción de Carrusel 1', 
-'Footer de Carrusel 1',
-'https://telegra.ph/file/24b24c495b5384b218b2f.jpg',
-[['Botón1', usedPrefix + 'menu'], ['Botón2', 'Id2'] /* etc... */],
-[['Texto para copiar 1'], ['Texto para copiar 2'] /* etc... */],
-[['Enlace1', canal2], ['Enlace2', 'https://example.com/link2'] /* etc... */],
-[['Botón Lista 1', sections], ['Botón Lista 2', sections] /* etc... */]
-], [ // CARRUSEL 2
-'Descripción de Carrusel 2',
-'Footer de Carrusel 2',
-'https://telegra.ph/file/e9239fa926d3a2ef48df2.jpg',
-[['Botón1', 'Id1'], ['Botón2', 'Id2']],
-[['Texto para copiar 1'], ['Texto para copiar 2']],
-[['Enlace1', 'https://example.com/link1'], ['Enlace2', 'https://example.com/link2']],
-[['Botón Lista 1', sections], ['Botón Lista 2', sections]]
-], [ // CARRUSEL 3
-'Descripción de Carrusel 3',
-'Footer de Carrusel 3',
-'https://telegra.ph/file/ec725de5925f6fb4d5647.jpg',
-[['Botón1', 'Id1'], ['Botón2', 'Id2']],
-[['Texto para copiar 1'], ['Texto para copiar 2']],
-[['Enlace1', 'https://example.com/link1'], ['Enlace2', 'https://example.com/link2']],
-[['Botón Lista 1', sections], ['Botón Lista 2', sections]]
-], [ // CARRUSEL 4
-'Descripción de Carrusel 4',
-'Footer de Carrusel 4',
-'https://telegra.ph/file/7acad0975febb71446da5.jpg',
-[['Botón1', 'Id1'], ['Botón2', 'Id2']],
-[['Texto para copiar 1'], ['Texto para copiar 2']],
-[['Enlace1', 'https://example.com/link1'], ['Enlace2', 'https://example.com/link2']],
-[['Botón Lista 1', sections], ['Botón Lista 2', sections]]
-]] /* etc... */
-await conn.sendCarousel(m.chat, 'Texto', 'Footer', 'Titulo de Carrusel', messages, m)            
-
+handler.before = async (m) => {
+let chat = global.db.data.chats[m.chat]
+if (chat.simi) {
+if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
+if (!m.text) return
+let textodem = m.text  
+try {
+await conn.sendPresenceUpdate('composing', m.chat)
+let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=` + lenguajeGB.lenguaje())
+let data = await ressimi.json();
+if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') return m.reply(`${lol}`) /* EL TEXTO "lol" NO ESTA DEFINIDO PARA DAR ERROR Y USAR LA OTRA API */
+await m.reply(data.success)
+} catch {
+/* SI DA ERROR USARA ESTA OTRA OPCION DE API DE IA QUE RECUERDA EL NOMBRE DE LA PERSONA */
+if (textodem.includes('Hola')) textodem = textodem.replace('Hola', 'Hello')
+if (textodem.includes('hola')) textodem = textodem.replace('hola', 'hello')
+if (textodem.includes('HOLA')) textodem = textodem.replace('HOLA', 'HELLO')    
+let reis = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=" + textodem)
+let resu = await reis.json()  
+let nama = m.pushName || '1'
+let api = await fetch("http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=" + nama + "&msg=" + resu[0][0][0])
+let res = await api.json()
+let reis2 = await fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=" + res.cnt)
+let resu2 = await reis2.json()
+await m.reply(resu2[0][0][0])}
+return !0
 }
-handler.command = /^(ترو)$/i
+return true
+}
 export default handler
