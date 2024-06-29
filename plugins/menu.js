8f3bbd919da9c1a1b6ba5265cbc2774414803dd2
mohmdali555555534 <lywnmalak@gmail.com>
2024-06-29 10:02:29 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,46 +1,41 @@
-import { createHash } from 'crypto'
-import PhoneNumber from 'awesome-phonenumber'
-import { canLevelUp, xpRange } from '../lib/levelling.js'
-import fetch from 'node-fetch'
-import fs from 'fs'
-const { levelling } = '../lib/levelling.js'
-import moment from 'moment-timezone'
-import { promises } from 'fs'
-import { join } from 'path'
+import fetch from 'node-fetch';
 
 
-let handler = async (m, { conn, usedPrefix, command }) => {
-  let d = new Date(new Date() + 3600000)
-  let locale = 'ar'
-  let week = d.toLocaleDateString(locale, { weekday: 'long' })
-  let date = moment().tz('Asia/Muscat').locale('ar').format('DD MMMM YYYY')
-  //let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
-  let _uptime = process.uptime() * 1000
-  let uptime = clockString(_uptime)
 
-  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  if (!(who in global.db.data.users)) throw `✳️ المستخدم غير موجود في قاعدة البيانات`
 
-  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
-  let user = global.db.data.users[who]
-  let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
-  let { min, xp, max } = xpRange(user.level, global.multiplier)
-  let username = conn.getName(who)
-  let math = max - xp
-  let prem = global.prems.includes(who.split`@`[0])
-  let sn = createHash('md5').update(who).digest('hex')
-  let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
-  let more = String.fromCharCode(8206)
-  let readMore = more.repeat(850)
-  let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-// الوقت باليمن
-  const time = moment.tz('Asia/Muscat').format('HH A')
-  let wib = moment.tz('Asia/Muscat').format('hh:mm:ss A')
-  
-  let str = `
-❆╼━━❆ •﹝📜﹞• ❆━━╾❆
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.menu_audios
 
-*↵ اهلا بك/ي "${name}"*
+  try {
+    const pp = imagen4;
+    // let vn = './media/menu.mp3'
+    const img = './Menu2.jpg';
+    const d = new Date(new Date + 3600000);
+    const locale = 'es';
+    const week = d.toLocaleDateString(locale, {weekday: 'long'});
+    const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
+    const _uptime = process.uptime() * 1000;
+    const uptime = clockString(_uptime);
+    const user = global.db.data.users[m.sender];
+    const {money, joincount} = global.db.data.users[m.sender];
+    const {exp, limit, level, role} = global.db.data.users[m.sender];
+    const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
+    const more = String.fromCharCode(8206);
+    const readMore = more.repeat(850);
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+    const document = doc[Math.floor(Math.random() * doc.length)];
+    const str = `╭═══〘 ✯✯✯✯✯✯✯✯✯ 〙══╮
+║    ◉— *بوت - الرعب الملكي* —◉
+║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
+║➤ *مرحبا, ${taguser}*
+║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
+╰═══╡✯✯✯✯✯✯✯✯✯╞═══╯
+
+
 
 『بـوت⤜الرعب الملكي』
 المطور الاساسي الرعب الملكي(https://wa.me/967700244383)
@@ -226,70 +221,27 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 المطور https://wa.me/967700244383
 اي مشكله تواجهك تعال للمطور يحلها لك
 ❋ ─━─⊰🍷⊱─━─ ❋
-*• ∙ ∙━━╍━﹝🌀﹞━╍━━∙ ∙ •*
-*❲ معلومات البوت ❳ ⬳ ⌝🤖⌞*
-*• ∙ ∙━━╍━﹝🌀﹞━╍━━∙ ∙ •*
-
-❒┇اسم البوت: *${botname}*
-❒┇اسم المالك: *${ownername}*
-❒┇نظام التشغيل: *لينكس*
-❒┇وقت التشغيل: *${uptime}*
-
-*• ∙ ∙━━╍━﹝🌀﹞━╍━━∙ ∙ •*
-*❲ معلومات المستخدم ❳ ⬳ ⌝👤⌞*
-*• ∙ ∙━━╍━﹝🌀﹞━╍━━∙ ∙ •*
-
-❒┇الإسم: *${name}*
-❒┇الحساب: *${taguser}*
-
-*• ∙ ∙━━╍━﹝🌀﹞━╍━━∙ ∙ •*
-*❲  التقویم ❳ ⬳ ⌝📅⌞*
-*• ∙ ∙━━╍━﹝🌀﹞━╍━━∙ ∙ •*
-
-*❍┇بتوقيت اليمن-🇾🇪-*
-❒┇الوقت: *${wib}*
-❒┇التاریخ: *${date}*
-
-*• ∙ ∙━━╍━﹝🌀﹞━╍━━∙ ∙ •*
-*❲ لعرض الاوامر اکتب/ي ❳*
-*⬳ ⌝ /المهام ⌞*
-*• ∙ ∙━━╍━﹝🌀﹞━╍━━∙ ∙ •*
-
-*بــــــ الملكي ــــــوت*
-
-❆╼━━❆ •﹝📜﹞• ❆━━╾❆`
-
-  conn.sendFile(m.chat, pp, 'darkman.png', str, m, false, { mentions: [who] })
-  m.react(done)
-}
-
-handler.help = ['main']
-handler.tags = ['group']
-handler.command = ['الأوامر', 'الاوامر', 'اوامر', 'أوامر','menu']
-
-export default handler
-
+┗━━━━━━━━━━━━━━━━┛`.trim();
+    if (m.isGroup) {
+      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+    } else {
+      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+    }
+  } catch {
+    conn.reply(m.chat, tradutor.texto2, m);
+  }
+};
+handler.command = /^(اوامر|الاوامر|أوامر|memu2|menuaudio|menuaudios|memuaudios|memuaudio|audios|keyaudio|keyaudios)$/i;
+handler.exp = 50;
+handler.fail = null;
+export default handler;
 function clockString(ms) {
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
-}
-
-function ucapan() {
-  const time = moment.tz('Asia/Muscat').format('HH A')
-  let res = "صباح الخير ☀️"
-  if (time >= 4) {
-    res = "صباح الخير 🌄"
-  }
-  if (time >= 10) {
-    res = "مساء الخير ☀️"
-  }
-  if (time >= 15) {
-    res = "مساء الخير 🌇"
-  }
-  if (time >= 18) {
-    res = "تصبح على خير 🌙"
-  }
-  return res
+  const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000);
+  const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
+  const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
+  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
 }
